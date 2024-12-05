e8ff50628219ff8e70416e63120aee7b2ade6a4a
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2024-12-05 17:00:04 +0100

    Remove useless rasp_init script

--- init_rasp/eee-disabling-eth0.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-while true
-do
-	/sbin/ethtool --set-eee eth0 eee off > /dev/null 2>&1
-	sleep 1
-done
--- init_rasp/setup_rasp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo mv ./eee-disabling-eth0.service /etc/systemd/system/eee-disabling-eth0.service
-sudo mv ./eee-disabling-eth0.sh /usr/local/sbin/eee-disabling-eth0.sh
-sudo systemctl enable eee-disabling-eth0.service
-sudo systemctl start eee-disabling-eth0.service
